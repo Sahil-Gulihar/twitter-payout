5fca590e4c8e6d18008103dc38c939a75e654882
Sahil Gulihar <sahilgullihar@gmail.com>
2026-01-18 18:13:41 +0530

    anal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react'
+import ReactGA from 'react-ga4'
 import { PaymentGenerator } from './components/PaymentGenerator'
 import './App.css'
 
+// Initialize Google Analytics
+ReactGA.initialize('G-7SPV6B9MPE')
+
 function App() {
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search })
+  }, [])
+
   return (
     <div className="app">
       <PaymentGenerator />
